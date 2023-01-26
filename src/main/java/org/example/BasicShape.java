2b34644cb6dd8eb21017683dd6ecd23a5a399dfb
ArunachalamTringapps <arunachalam.muthuvel@tringapps.com>
2023-01-26 09:49:44 +0530

    final changes

--- src/main/java/org/example/BasicShape.java
+++ src/main/java/org/example/BasicShape.java
@@ -11,7 +11,7 @@ class BasicShape {
     private final double s1;
     private final double s2;
     private final double radius;
-    private final double pi = 3.14;
+    public static final double pi = 3.14;
 
     public BasicShape(int type, double length, double breadth, double side1, double side2, double radius) {
         this.type=type;
@@ -51,20 +51,31 @@ class BasicShape {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome guys");
-        System.out.println("choose one from below.");
+        System.out.println("Enter your choice");
         System.out.println("1.Rectangle\n2.Triangle\n3.Circle");
         System.out.println("Enter the shape type:");
         int type = sc.nextInt();
-        System.out.println("Enter the length:");
-        double length = sc.nextFloat();
-        System.out.println("Enter the breadth:");
-        double breadth = sc.nextFloat();
-        System.out.println("Enter the side1:");
-        double side1 = sc.nextFloat();
-        System.out.println("Enter the side2:");
-        double side2 = sc.nextFloat();
-        System.out.println("Enter the radius:");
-        double radius = sc.nextFloat();
+        double radius=0;
+        double length = 0;
+        double breadth=0;
+        double side1=0;
+        double side2=0;
+        if(type==3){
+            System.out.println("Enter the radius:");
+            radius = sc.nextFloat();
+        }
+        else if (type==1 || type==2){
+            System.out.println("Enter the length:");
+            length = sc.nextFloat();
+            System.out.println("Enter the breadth:");
+            breadth = sc.nextFloat();
+            System.out.println("Enter the side1:");
+            side1 = sc.nextFloat();
+            System.out.println("Enter the side2:");
+            side2 = sc.nextFloat();
+        }
+
+
 
         BasicShape bs = new BasicShape(type, length, breadth, side1, side2, radius);
         System.out.println("The perimeter of the given shape is:" + bs.perimeter());
